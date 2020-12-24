b1bf0fbfb634cb7b43fb3b8472f2602a41922da7
Kris Verbeeck <kris_verbeeck@fastmail.fm>
2020-12-24 08:14:06 +0100

    day 24 (clean-up)

--- day24.py
+++ day24.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from typing import List, Tuple, Set, Dict, Callable
+from typing import List, Tuple, Dict, Callable
 
 from day17 import GameOfLife, State
 from util import load
@@ -8,74 +8,50 @@ Coord = Tuple[int, int]
 Grid = Dict[Coord, State]
 
 
-#       /  \  /  \
-#      | NW| | NE|
-#   /  \  /  \  /  \
-#  | W | | O | | E |
-#  \  /  \  /  \  /
-#    | SW| | SE|
-#    \  /  \  /
-#
-def flip(lines: List[str]) -> Set[Coord]:
-    blacks: Set[Coord] = set()
-    for line in lines:
-        x, y = 0, 0
-        while line:
-            if line.startswith('e'):
-                x, y = x + 2, y
-                line = line[1:]
-            elif line.startswith('se'):
-                x, y = x + 1, y - 1
-                line = line[2:]
-            elif line.startswith('sw'):
-                x, y = x - 1, y - 1
-                line = line[2:]
-            elif line.startswith('w'):
-                x, y = x - 2, y
-                line = line[1:]
-            elif line.startswith('nw'):
-                x, y = x - 1, y + 1
-                line = line[2:]
-            elif line.startswith('ne'):
-                x, y = x + 1, y + 1
-                line = line[2:]
-        if (x, y) in blacks:
-            blacks.remove((x, y))
-        else:
-            blacks.add((x, y))
-    return blacks
-
-
 class GameOfLifeHex(GameOfLife):
+    TOKENS: Dict[str, Coord] = {'e': (2, 0), 'se': (+1, -1), 'sw': (-1, -1),
+                                'w': (-2, 0), 'nw': (-1, 1), 'ne': (1, 1)}
+
     def __init__(self, lines: List[str]):
         GameOfLife.__init__(self, [], activate={2}, inactivate={1, 2})
-        blacks = flip(lines)
-        for coord in blacks:
-            self._set_activity(coord, True)
+        self._flip(lines)
 
     def new_coord(self, x: int, y: int) -> Coord:
         return x, y
 
     def update_neighbours(self, coord: Coord, f: Callable[[int], int]):
         cx, cy = coord
-        for x, y in [(2, 0), (+1, -1), (-1, -1), (-2, 0), (-1, 1), (1, 1)]:
+        for x, y in self.TOKENS.values():
             c = (cx + x, cy + y)
             self.update_neighbour(c, f)
+
+    def _flip(self, lines: List[str]):
+        for line in lines:
+            x, y = 0, 0
+            while line:
+                for direction, (dx, dy) in self.TOKENS.items():
+                    if line.startswith(direction):
+                        x, y = x + dx, y + dy
+                        line = line[len(direction):]
+                        break
+            _, active = self._grid.get((x, y), (0, False))
+            if active:
+                self._set_activity((x, y), False)
+            else:
+                self._set_activity((x, y), True)
 
 
 if __name__ == "__main__":
     data = load('day24-test.txt')
-    assert len(flip(data)) == 10
     gol = GameOfLifeHex(data)
+    assert gol.active() == 10, gol.active()
     gol.evolve(100)
     assert gol.active() == 2208
 
     data = load('day24.txt')
-    the_blacks = flip(data)
-    assert len(the_blacks) == 287
-    print('Number of black tiles is %d' % len(the_blacks))
-
     gol = GameOfLifeHex(data)
+    assert gol.active() == 287
+    print('Number of black tiles is %d' % gol.active())
     gol.evolve(100)
     assert gol.active() == 3636
     print('Number of black tiles after 100 generations is %d' % gol.active())
